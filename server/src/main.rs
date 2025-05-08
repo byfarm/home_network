5eb4e1ee81567866e1aadec39a62417d5ee84a0c
Byron Farmar <buckshootroger@gmail.com>
2025-05-08 09:42:43 -0700

    used arc instead of box

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,28 +12,40 @@ async fn main() -> Result<(), std::io::Error> {
 
     let mut buf = [0; BUFFER_SIZE];
 
-    let pool = Box::new(
-        PoolBuilder::new()
-            .path("db.sqlite3")
-            .journal_mode(JournalMode::Wal)
-            .open()
-            .await
-            .unwrap(),
-    );
+    let pool = PoolBuilder::new()
+        .path("db.sqlite3")
+        .journal_mode(JournalMode::Wal)
+        .open()
+        .await
+        .expect("Unable to open new database pool.");
+
+    let shared_pool = std::sync::Arc::new(pool);
 
     loop {
         let (len, addr) = sock.recv_from(&mut buf).await?;
         println!("Recieved message of length: {}, from: {}", len, addr);
-        let recieved_data = NetworkPacket::from_bytes(&buf).unwrap();
 
-        handle_data(pool.clone(), recieved_data).await.unwrap();
+        let recieved_data =
+            NetworkPacket::from_bytes(&buf).expect("Unable to decode bytes into a readable state.");
+
+        let pool_clone = shared_pool.clone();
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_data(pool_clone, recieved_data).await {
+                eprintln!("Could not handle data from {}: {}", addr, e);
+            };
+        });
     }
 }
 
-async fn handle_data(pool: Box<Pool>, packet: NetworkPacket) -> Result<(), std::io::Error> {
-    pool.conn(move |conn| {
-        conn.execute(
-            "INSERT INTO
+async fn handle_data(
+    pool: std::sync::Arc<Pool>,
+    packet: NetworkPacket,
+) -> Result<(), async_sqlite::Error> {
+    match pool
+        .conn(move |conn| {
+            conn.execute(
+                "INSERT INTO
                 data (location_id, value, measurand, units)
             SELECT
                 location.id,
@@ -44,16 +56,22 @@ async fn handle_data(pool: Box<Pool>, packet: NetworkPacket) -> Result<(), std::
             WHERE
                 location.name = ?1
             LIMIT 1",
-            (
-                packet.location,
-                *packet.data.first().unwrap(),
-                "temperature",
-                packet.units,
-            ),
-        )
-    })
-    .await
-    .unwrap();
-
-    Ok(())
+                async_sqlite::rusqlite::params![
+                    "kitchen",
+                    *packet.data.first().unwrap(),
+                    "temperature",
+                    packet.units,
+                ],
+            )
+        })
+        .await
+    {
+        Ok(v) => {
+            println!("uploaded data to database! rows affected: {}", v);
+            Ok(())
+        }
+        Err(e) => {
+            Err(e)
+        }
+    }
 }
